71d1bffef5247893260de661d7352ad014e18012
Isaac Boampongsem <isaac.boampongsem@gtnllc.com>
2023-09-27 12:56:02 +0100

    app key

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,7 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-# echo "generating application key..."
+echo "generating application key..."
 php artisan key:generate --show
 
 echo "Caching config..."
